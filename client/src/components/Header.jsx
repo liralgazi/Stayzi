9e651284b8b9fe1b04746cbd6decb82b5d3b38c9
Lir Algazi <liralgazi@gmail.com>
2023-06-04 15:04:24 +0300

    design changes on header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,8 @@ export default function Header()
 {
     return(
         <header className='flex justify-between'>
-          <img src="icons/stayzi-logo-7.png" className="w-50 h-20"/>
-        <div className='center-items flex gap-3 border border-gray-300 rounded-full py-5 px-3 font-bold shadow-md shadow-gray-300'>
+          <img src="icons/stayzi-logo-7.png" className="w-50 h-20 "/>
+        <div className='items-center flex gap-3 border border-gray-300 rounded-full py-5 px-3 font-bold shadow-md shadow-gray-300'>
           <div>Anywhere</div>
           <div className="border-l border-gray-300"></div>
           <div>Any week</div>
@@ -20,7 +20,7 @@ export default function Header()
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
-          <div className='bg-gray-400 text-white rounded-full rounded-full overflow-hidden'>
+          <div className='bg-primary text-white rounded-full rounded-full overflow-hidden'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 relative -bottom-1">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
