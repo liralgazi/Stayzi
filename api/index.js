f6255917ca85a0e0735d6abcf5083bd0ad3be98d
Lir Algazi <liralgazi@gmail.com>
2023-06-05 10:32:36 +0300

    connection to mongo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express();
+
+require('dotenv').config()
 
 app.use(express.json());
 
@@ -10,10 +13,15 @@ app.use(cors({
     
 }));
 
+console.log(process.env.MONGO_URL)
+console.log('here')
+
+//connecting to mongoDB 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/test', (req,res) => {
     res.json('test ok')
 });
-
 
 app.post('/signup', (req,res) => { 
     const {name, email, password} = req.body;
